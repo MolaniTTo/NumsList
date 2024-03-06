3c7bf284418223d704a7d09472b87f383e54893f
arnauitb <arnau.molano.7e7@itb.cat>
2024-03-06 11:22:39 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace IntList
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            List<int> list = new List<int>
+            {
+                1, 2, 3, 4, 5, 6
+            };
+
+            int suma = 0;
+
+            foreach (int i in list)
+            {
+                suma+=i;       
+            }
+
+            Console.WriteLine(suma);
+
+
+           
+           
+
+
+
+
+        }
+    }
+}
